be81ccf009df7ac71c0f9b8beb04cef625fdc280
BasemNagah <basem3nagah@gmail.com>
2024-12-04 15:19:33 +0300

    Published

---
+++ PublishedProject/wwwroot/assets/js/myJS.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var currentPath = window.location.pathname;
+    var links = document.querySelectorAll(".dropdown a");
+
+    links.forEach(function (link) {
+        if (link.getAttribute("href") === currentPath) {
+            link.classList.add("active");
+            document.getElementById("services-link").classList.add("active");
+        }
+    });
+});
+
